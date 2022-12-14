9be7a96441d10924ba042c4e73cb2b43864db123
신흥재 <xfp.730700@gmail.com>
2022-12-14 16:04:37 +0900

    CoreUI 4.1 + Vite 4.0.0

---
+++ src/components/DocsCallout.vue
@@ -0,0 +1,53 @@
+<template>
+  <CCallout color="info" class="bg-white">
+    {{
+      content
+        ? content
+        : `A Vue ${name} component ${
+            plural ? 'have' : 'has'
+          } been created as a native Vue.js version
+      of Bootstrap ${name}. ${name} ${
+            plural ? 'are' : 'is'
+          } delivered with some new features,
+      variants, and unique design that matches CoreUI Design System requirements.`
+    }}
+    <br />
+    <br />
+    For more information please visit our official
+    <CLink :href="url" target="_blank">
+      documentation of CoreUI Components Library for Vue.js
+    </CLink>
+    .
+  </CCallout>
+</template>
+
+<script>
+export default {
+  name: 'DocsCallout',
+  props: {
+    content: {
+      type: String,
+      default: undefined,
+      required: false,
+    },
+    href: {
+      type: String,
+      default: undefined,
+      required: false,
+    },
+    name: {
+      type: String,
+      default: undefined,
+      required: false,
+    },
+    plural: Boolean,
+  },
+  setup(props) {
+    const url = `https://coreui.io/vue/docs/4.2/${props.url}`
+
+    return {
+      url,
+    }
+  },
+}
+</script>
